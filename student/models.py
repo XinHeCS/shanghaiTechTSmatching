4afc3de8eb5663c05e8325565ecf21dea7a5dc3f
dry950526@gmail.com <dry950526@gmail.com>
2017-07-12 15:39:16 +0800

    add models

--- student/models.py
+++ student/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    id = models.CharField(primary_key=True, max_length=18)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=25)
+    university = models.CharField(max_length=25)
+    gpa = models.FloatField()
+    ranking = models.FloatField()
+    comment = models.TextField()
+    attachment = models.FileField()
+    first_teacher = models.CharField(max_length=20)
+    second_teacher = models.CharField(max_length=20)
+    third_teacher = models.CharField(max_length=20)
+
+class Selection(models.Model):
+    student = models.ManyToManyField(Students,related_name="student")
+    first_teacher = models.ManyToManyField('teacher.Teachers', related_name="teacher1")
+    second_teacher = models.ManyToManyField('teacher.Teachers', related_name="teacher2")
+    third_teacher = models.ManyToManyField('teacher.Teachers', related_name="teacher3")
+
