8e829fc67a576027f9e41e7c3f309f4d780dd69b
dry950526@gmail.com <dry950526@gmail.com>
2017-07-12 18:24:08 +0800

    "models tools"

--- student/forms.py
+++ student/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class LoginForm(forms.Form):
     user_name = forms.CharField(label='User:', max_length='50', required=True, initial='Enter your name', strip=True)
     user_pwd = forms.CharField(label='Password:', widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+    user_name = forms.CharField(label='User:', max_length='50', required=True, initial='Enter your name', strip=True)
+    user_pwd = forms.CharField(label='Password:', widget=forms.PasswordInput)
+    user_pwd_confirm = forms.CharField(label='Password:', widget=forms.PasswordInput)
--- student/model_tools/check_stu.py
+++
@@ -1,7 +0,0 @@
-from ..models import Students
-
-class StudentValidator(Students):
-    # test function
-    def can_login(self, user_name, user_pwd):
-        return True
-
