e608fe7f19c5a45cbcbf24d8eb15200890df3daa
dry950526@gmail.com <dry950526@gmail.com>
2017-07-13 15:06:03 +0800

    form

--- student/forms.py
+++ student/forms.py
@@ -27,3 +27,26 @@ class RegisterForm(forms.Form):
         'class': 'form-control',
         'placeholder': 'Repeat your password'
     }))
+
+class EditForm(forms.Form):
+    stu_name = forms.CharField(label='姓名:', max_length='20', required=True, strip=True,
+    widget = forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': '输入姓名'
+    })
+    )
+    stu_id = forms.CharField(label='身份证号：:'
+                             , max_length='18', required=True, strip=True,
+                             widget=forms.TextInput(attrs={
+                                 'class': 'form-control',
+                                 'placeholder': '输入身份证号'
+                             })
+                             )
+    stu_sex = forms.BooleanField(label='性别', required=True)
+    stu_birth = forms.DateField(label='出生日期:', widget=forms.DateInput(attrs={
+        'class': 'form-control',
+        'placeholder': '年-月-日'
+    }))
+
+
+c
