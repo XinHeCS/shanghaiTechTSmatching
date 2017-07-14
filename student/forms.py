e4cc5253a725d44f81eabc8aa6e1b0f9c994a0d8
dry950526@gmail.com <dry950526@gmail.com>
2017-07-14 13:12:45 +0800

    user authenticate

--- student/forms.py
+++ student/forms.py
@@ -23,9 +23,13 @@ class RegisterForm(forms.Form):
         'class': 'form-control',
         'placeholder': 'Enter your password'
     }))
-    user_pwd_confirm = forms.CharField(label='Password:', widget=forms.PasswordInput(attrs={
+    user_pwd_confirm = forms.CharField(label='Confirm your password :', widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'placeholder': 'Repeat your password'
+    }))
+    user_email = forms.CharField(label='Email:', widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email address'
     }))
 
 class EditForm(forms.Form):
@@ -35,14 +39,14 @@ class EditForm(forms.Form):
         'placeholder': '输入姓名'
     })
     )
-    stu_id = forms.CharField(label='身份证号：:'
+    stu_id = forms.CharField(label='身份证号：'
                              , max_length='18', required=True, strip=True,
                              widget=forms.TextInput(attrs={
                                  'class': 'form-control',
                                  'placeholder': '输入身份证号'
                              })
                              )
-    stu_sex = forms.ChoiceField(label='性别', required=True, widget=forms.RadioSelect, choices=(('1', '男',), ('0', '女',)))
+    stu_sex = forms.ChoiceField(label='性别:', required=True, widget=forms.Select(attrs={'class':'form-control'}), choices=(('1', '男',), ('0', '女',)))
     stu_birth = forms.DateField(label='出生日期:', widget=forms.DateInput(attrs={
         'class': 'form-control',
         'placeholder': '年-月-日'
@@ -50,10 +54,11 @@ class EditForm(forms.Form):
     stu_email = forms.EmailField(label='电子邮箱：', required=True, widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'邮箱'}))
     stu_phone_number = forms.CharField(label='手机号：',max_length=25, required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'手机号码'}))
     stu_university = forms.CharField(label='本科学校：',max_length=25, required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'本科学校名称'}))
+    stu_major = forms.CharField(label='本科专业：', required=True,
+                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '填写本科专业名称'}))
     stu_gpa = forms.FloatField(label='GPA/学分绩：', required=True, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'百分制学分绩，如没有，使用4分制GPA'}))
     stu_ranking = forms.CharField(label='专业排名（百分比）：', required=True, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'专业排名百分比'}))
-
-    stu_attachment = forms.FileField(label='附件（简历等其他支撑材料,可选）', )
-    stu_comment = forms.CharField(label='备注（可选）',widget=forms.TextInput(attrs={'class':'form-control','placeholder':'如有其他信息请备注'}))
+    stu_attachment = forms.FileField(label='附件（简历等其他支撑材料,可选）',widget=forms.FileInput(attrs={'class':'form-control'}))
+    stu_comment = forms.CharField(label='备注（可选）',widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'如有其他信息请备注'}))
 
 
