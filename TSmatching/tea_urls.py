89442e80c8c4339ab9de97bc89201f4e8ffd0a38
XinHeCS <xinhe_cs@163.com>
2017-09-14 15:28:31 +0800

    Enable foreign students to apply

--- TSmatching/tea_urls.py
+++ TSmatching/tea_urls.py
@@ -3,7 +3,7 @@ from . import tea_views
 
 urlpatterns = [
     url(r'^login/$', tea_views.login, name='tea_login_page'),
-    url(r'^messages/(accept|reject|reject_ac)/(\w+)/$', tea_views.message_page, name='tea_mess_page'),
+    url(r'^messages/(accept|reject|reject_ac)/(\s*)(\w+)(\s*)/$', tea_views.message_page, name='tea_mess_page'),
     url(r'^main_page/$', tea_views.main_page, name='tea_main_page'),
     url(r'^log_out/$', tea_views.logout, name='tea_logout')
 ]
